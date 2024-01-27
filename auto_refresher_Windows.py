346d21bffd9d13a05cfd9c348ab6bf85b7e7981c
Daniel Perodin <perodindaniel@gmail.com>
2024-01-26 20:33:43 -0500

    Made the scraper return a list of urls.

--- auto_refresher_Windows.py
+++ auto_refresher_Windows.py
@@ -4,12 +4,13 @@ import os
 from scraper import check_keywords
 
 def main():
+    urls = []
     while True:
         time.sleep(1)
         pyautogui.keyDown('ctrl')
         pyautogui.press('r')
         pyautogui.keyUp('ctrl')
-        check_keywords()
+        check_keywords(urls)
 
 if __name__ == '__main__':
     main()
--- scraper.py
+++ scraper.py
@@ -3,19 +3,18 @@ import time
 from bs4 import BeautifulSoup
 import requests
 
-def check_keywords():
+def check_keywords(urls):
     url = 'https://www.palmettopanthers.org'
     reqs = requests.get(url)
     soup = BeautifulSoup(reqs.text, 'html.parser')
  
-    urls = []
-    links = 0
     for link in soup.find_all('a'):
         if(link.get('href')[:8] == 'https://'):
-            links += 1
-            print(link.get('href'))
-    print(links)
+            urls.append(link.get('href'))
+            #print(link.get('href'))
+    
+    return urls
     
 
 if __name__ == '__main__':
-    check_keywords()
+    print(check_keywords([]))
