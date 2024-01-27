f8d8d22620ac7703dba776865e96dae835c45379
Daniel Perodin <perodindaniel@gmail.com>
2024-01-26 21:20:43 -0500

    Added print statements to indicate progress.

--- auto_refresher.py
+++ auto_refresher.py
@@ -5,8 +5,11 @@ from scraper import check_keywords
 def main():
     urls_old = []
     running = True
+    print('beginning to search school website')
+    iterations = 0
     while running:
-        time.sleep(1)
+        iterations += 1
+        time.sleep(0.5)
         urls_new = check_keywords()
         if (urls_new != urls_old and not urls_old):
             for urls in urls_old:
@@ -15,6 +18,8 @@ def main():
                 except ValueError:
                    webbrowser.open(urls)
                    running = False
+        if iterations%20 == 0:
+            print('still looking')
 
 
 if __name__ == '__main__':
