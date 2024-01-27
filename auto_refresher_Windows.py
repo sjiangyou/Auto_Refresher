bc106c117264b8747515e81033f6e8956edacbdf
Sunny You <sunny.jiang.you@gmail.com>
2024-01-26 20:15:49 -0500

    Seperated code for Mac and Windows.

---
+++ auto_refresher_Windows.py
@@ -0,0 +1,14 @@
+import time
+import pyautogui
+import os
+from scraper import check_keywords
+
+def main():
+    while True:
+        time.sleep(1)
+        pyautogui.keyDown('ctrl')
+        pyautogui.press('r')
+        pyautogui.keyUp('ctrl')
+
+if __name__ == '__main__':
+    main()
